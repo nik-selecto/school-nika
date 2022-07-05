dd8f92e85506053af92eb6e73325ed0f5fb55720
nik-selecto <n.moiseienko@selectoglobal.com>
2022-07-05 23:23:22 +0300

    refactor(root): add eslint

--- lessons/event-emitter/__tests__/event-emitter.test.js
+++ lessons/event-emitter/__tests__/event-emitter.test.js
@@ -2,44 +2,45 @@
 const Emitter = require('events');
 
 describe('event-emitter', () => {
-    let ok, hello;
-    let tester = [];
+  let ok; let
+    hello;
+  let tester = [];
 
-    beforeEach(() => {
-        ok = new Emitter();
-        hello = new Emitter();
-        tester = [];
-        hello.on('hello', () => tester.push('world'));
-        ok.on('ok', () => tester.push('google'));
-    });
+  beforeEach(() => {
+    ok = new Emitter();
+    hello = new Emitter();
+    tester = [];
+    hello.on('hello', () => tester.push('world'));
+    ok.on('ok', () => tester.push('google'));
+  });
 
-    it('Should store "world" keys on "hello" event', () => {
-        hello.emit('hello');
+  it('Should store "world" keys on "hello" event', () => {
+    hello.emit('hello');
 
-        expect(tester.pop()).toBe('world');
-    });
+    expect(tester.pop()).toBe('world');
+  });
 
-    it('Should store "google" keys on "ok" event', () => {
-        ok.emit('ok');
+  it('Should store "google" keys on "ok" event', () => {
+    ok.emit('ok');
 
-        expect(tester.pop()).toBe('google');
-    });
+    expect(tester.pop()).toBe('google');
+  });
 
-    it('Should store "world" 2 times and "google" -3', () => {
-        hello.emit('hello');
-        hello.emit('hello');
-        ok.emit('ok');
-        ok.emit('ok');
-        ok.emit('ok');
+  it('Should store "world" 2 times and "google" -3', () => {
+    hello.emit('hello');
+    hello.emit('hello');
+    ok.emit('ok');
+    ok.emit('ok');
+    ok.emit('ok');
 
-        ['world', 'world', 'google', 'google', 'google'].forEach((v, i) => expect(v).toBe(tester[i]));
-    });
+    ['world', 'world', 'google', 'google', 'google'].forEach((v, i) => expect(v).toBe(tester[i]));
+  });
 
-    it('Should store 100 items', () => {
-        for (let i = 0; i < 100; ++i) {
-            hello.emit('hello');
-        }
+  it('Should store 100 items', () => {
+    for (let i = 0; i < 100; ++i) {
+      hello.emit('hello');
+    }
 
-        expect(tester.length).toBe(100);
-    });
+    expect(tester.length).toBe(100);
+  });
 });
